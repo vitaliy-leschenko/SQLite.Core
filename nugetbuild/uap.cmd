0da0dd361c9ee58608d2da1ca191c4d6e905ceb3
Виталий Лещенко <v.leschenko@vitaliy.org>
2015-08-04 10:59:20 +0300

    nuget

---
+++ nugetbuild/uap.cmd
@@ -0,0 +1,4 @@
+NuGet.exe pack SQLite.Core.Universal-arm.nuspec
+NuGet.exe pack SQLite.Core.Universal-x64.nuspec
+NuGet.exe pack SQLite.Core.Universal-x86.nuspec
+NuGet.exe pack SQLite.Core.Universal.nuspec

9fb3138c64e8a0ab816bc6349ff28b77aef17137
Виталий Лещенко <v.leschenko@vitaliy.org>
2015-08-04 18:38:38 +0300

    Build SDK as temp solution for using SQLite.Core

---
+++ SDK/buildsdk.ps1
@@ -0,0 +1,21 @@
+if (!(Test-Path 'src\Redist\CommonConfiguration\ARM\')) { New-Item -ItemType directory -Path 'src\Redist\CommonConfiguration\ARM\' }
+if (!(Test-Path 'src\Redist\CommonConfiguration\x86\')) { New-Item -ItemType directory -Path 'src\Redist\CommonConfiguration\x86\' }
+if (!(Test-Path 'src\Redist\CommonConfiguration\x64\')) { New-Item -ItemType directory -Path 'src\Redist\CommonConfiguration\x64\' }
+if (!(Test-Path 'src\References\CommonConfiguration\ARM\')) { New-Item -ItemType directory -Path 'src\References\CommonConfiguration\ARM\' }
+if (!(Test-Path 'src\References\CommonConfiguration\x86\')) { New-Item -ItemType directory -Path 'src\References\CommonConfiguration\x86\' }
+if (!(Test-Path 'src\References\CommonConfiguration\x64\')) { New-Item -ItemType directory -Path 'src\References\CommonConfiguration\x64\' }
+
+Copy-Item ..\SQLite.Core\SQLite.Core.Universal\bin\ARM\Release\SQLite.Core.dll src\Redist\CommonConfiguration\ARM\SQLite.Core.dll
+Copy-Item ..\SQLite.Core\SQLite.Core.Universal\bin\Win32\Release\SQLite.Core.dll src\Redist\CommonConfiguration\x86\SQLite.Core.dll
+Copy-Item ..\SQLite.Core\SQLite.Core.Universal\bin\x64\Release\SQLite.Core.dll src\Redist\CommonConfiguration\x64\SQLite.Core.dll
+Copy-Item ..\SQLite.Core\SQLite.Core.Universal\bin\ARM\Release\SQLite.Core.winmd src\References\CommonConfiguration\ARM\SQLite.Core.winmd
+Copy-Item ..\SQLite.Core\SQLite.Core.Universal\bin\Win32\Release\SQLite.Core.winmd src\References\CommonConfiguration\x86\SQLite.Core.winmd
+Copy-Item ..\SQLite.Core\SQLite.Core.Universal\bin\x64\Release\SQLite.Core.winmd src\References\CommonConfiguration\x64\SQLite.Core.winmd
+
+if (Test-Path 'SQLite.Core.SDK.vsix')
+{
+  Remove-Item 'SQLite.Core.SDK.vsix'
+}
+
+Add-Type -A System.IO.Compression.FileSystem
+[IO.Compression.ZipFile]::CreateFromDirectory('src', 'SQLite.Core.SDK.vsix')

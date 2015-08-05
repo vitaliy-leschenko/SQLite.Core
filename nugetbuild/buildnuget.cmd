660a8067fb1c2fa169d94ff70f204077b69d8f74
Виталий Лещенко <v.leschenko@vitaliy.org>
2015-08-06 00:57:30 +0300

    Create NuGet packages for UAP

--- nugetbuild/buildnuget.cmd
+++ nugetbuild/buildnuget.cmd
@@ -1,3 +1,3 @@
-NuGet.exe pack SQLite.Core.Universal.nuspec
+NuGet.exe pack SQLite.Core.UAP.nuspec
 NuGet.exe pack SQLite.Core.Windows.nuspec
 NuGet.exe pack SQLite.Core.WindowsPhone.nuspec
--- nugetbuild/uap.cmd
+++
@@ -1,4 +0,0 @@
-NuGet.exe pack SQLite.Core.Universal-arm.nuspec
-NuGet.exe pack SQLite.Core.Universal-x64.nuspec
-NuGet.exe pack SQLite.Core.Universal-x86.nuspec
-NuGet.exe pack SQLite.Core.Universal.nuspec
